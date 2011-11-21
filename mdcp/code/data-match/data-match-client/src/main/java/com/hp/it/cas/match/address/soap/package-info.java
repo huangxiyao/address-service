8980883ff513ee25d96d4d7f927aa97fe78b7d64
Paul Truax <paul.truax@hp.com>
2011-11-21 22:14:47 +0000

    Added deprecated javadoc and annotation
    git-svn-id: file:///opt/mount/casfw/ad-git-migration/mdcp-svn/trunk@1674 3ae50711-85f4-4630-9f8a-40507e8d37d8

--- mdcp/code/data-match/data-match-client/src/main/java/com/hp/it/cas/match/address/soap/package-info.java
+++ mdcp/code/data-match/data-match-client/src/main/java/com/hp/it/cas/match/address/soap/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Soap specific client components
+ * Legacy Soap specific client components.  Do not use for new initiatives.
  */
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://localhost/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.hp.it.cas.match.address.soap;

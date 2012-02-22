4dfc32f21097541a8169a37eaf22f7aca74a174f
Quintin May <quintin.may@hp.com>
2012-02-22 17:46:31 +0000

    Add PreferredScript.
    git-svn-id: file:///opt/mount/casfw/ad-git-migration/mdcp-svn/trunk@2642 3ae50711-85f4-4630-9f8a-40507e8d37d8

---
+++ mdcp/code/data-match/data-match-engine/src/main/java/com/hp/it/cas/match/address/engine/PreferredScript.java
@@ -0,0 +1,53 @@
+package com.hp.it.cas.match.address.engine;
+
+import static com.hp.it.cas.match.address.engine.CharacterScript.GREEK;
+import static com.hp.it.cas.match.address.engine.CharacterScript.HAN;
+import static com.hp.it.cas.match.address.engine.CharacterScript.LATIN;
+import static com.hp.it.cas.match.address.engine.PreferredScript.AddressDoctorScript.ASCII_EXTENDED;
+import static com.hp.it.cas.match.address.engine.PreferredScript.AddressDoctorScript.POSTAL_ADMIN_PREF;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.hp.it.cas.match.address.AddressQuery;
+
+class PreferredScript {
+
+    private static final Map<String, Map<CharacterScript, AddressDoctorScript>> countryScripts = new HashMap<String, Map<CharacterScript, AddressDoctorScript>>();
+    
+    enum AddressDoctorScript {
+        POSTAL_ADMIN_PREF,
+        ASCII_EXTENDED,
+        POSTAL_ADMIN_ALT
+    }
+    
+    static {
+        map("CN", HAN, POSTAL_ADMIN_PREF);
+        map("CN", LATIN, ASCII_EXTENDED);
+        map("GR", GREEK, POSTAL_ADMIN_PREF);
+        map("GR", LATIN, ASCII_EXTENDED);
+        
+        // TODO more mappings
+    }
+
+    static String preferredScript(AddressQuery query) {
+        AddressDoctorScript addressDoctorScript = null;
+        if (query.getCharacterScriptDetectionIndicator()) {
+            Map<CharacterScript, AddressDoctorScript> scriptMapping = countryScripts.get(query.getCountry1());
+            if (scriptMapping != null) {
+                CharacterScript detectedScript = CharacterScript.detect(query.getAddressValues());
+                addressDoctorScript = scriptMapping.get(detectedScript);
+            }
+        }
+        return (addressDoctorScript == null ? POSTAL_ADMIN_PREF : addressDoctorScript).name();
+    }
+    
+    private static void map(String countryCode, CharacterScript characterScript, AddressDoctorScript addressDoctorScript) {
+        Map<CharacterScript, AddressDoctorScript> scriptMapping = countryScripts.get(countryCode);
+        if (scriptMapping == null) {
+            scriptMapping = new HashMap<CharacterScript, AddressDoctorScript>();
+            countryScripts.put(countryCode, scriptMapping);
+        }
+        scriptMapping.put(characterScript, addressDoctorScript);
+    }    
+}

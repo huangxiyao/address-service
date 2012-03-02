bbe5b78470372842c04ea898ec9c553edac87f02
Santhosh Chandan <santhosh.chandan@hp.com>
2012-03-02 16:38:18 +0000

    PreferredScript
    git-svn-id: file:///opt/mount/casfw/ad-git-migration/mdcp-svn/trunk@2804 3ae50711-85f4-4630-9f8a-40507e8d37d8

--- mdcp/code/data-match/data-match-engine/src/main/java/com/hp/it/cas/match/address/engine/PreferredScript.java
+++ mdcp/code/data-match/data-match-engine/src/main/java/com/hp/it/cas/match/address/engine/PreferredScript.java
@@ -23,28 +23,36 @@ class PreferredScript {
     static {
         addMapping("CN", HAN,      POSTAL_ADMIN_PREF);
         addMapping("CN", CharacterScript.LATIN,    ASCII_EXTENDED);
+        addMapping("CN", null,    ASCII_EXTENDED);
         
         addMapping("GR", GREEK,    POSTAL_ADMIN_PREF);
         addMapping("GR", CharacterScript.LATIN,    ASCII_EXTENDED);
+        addMapping("GR", null,    ASCII_EXTENDED);
         
         addMapping("HK", HAN,    POSTAL_ADMIN_PREF);
         addMapping("HK", CharacterScript.LATIN,    ASCII_EXTENDED);
+        addMapping("HK", null,    ASCII_EXTENDED);
         
         addMapping("IL", HEBREW,    POSTAL_ADMIN_PREF);
         addMapping("IL", CharacterScript.LATIN,    ASCII_EXTENDED);
+        addMapping("IL", null,    ASCII_EXTENDED);
         
         addMapping("JP", CharacterScript.JAPAN,    POSTAL_ADMIN_PREF);
         addMapping("JP", CharacterScript.KATAKANA,    POSTAL_ADMIN_ALT);
         addMapping("JP", CharacterScript.LATIN,    ASCII_EXTENDED);
+        addMapping("JP", null,    ASCII_EXTENDED);
         
         // addMapping("KO", ,    POSTAL_ADMIN_PREF);
         addMapping("KO", CharacterScript.LATIN,    ASCII_EXTENDED);
+        addMapping("KO", null,    ASCII_EXTENDED);
 
         addMapping("RU", CharacterScript.CYRILLIC,    POSTAL_ADMIN_PREF);
         addMapping("RU", CharacterScript.LATIN,    ASCII_EXTENDED);
+        addMapping("RU", null,    ASCII_EXTENDED);
         
         addMapping("TW", HAN,    POSTAL_ADMIN_PREF);
         addMapping("TW", CharacterScript.LATIN,    ASCII_EXTENDED);
+        addMapping("TW", null,    ASCII_EXTENDED);
      }
 
     private PreferredScript() {}
@@ -63,6 +71,7 @@ class PreferredScript {
             if (scriptMapping != null) {
                 CharacterScript detectedScript = CharacterScript.detect(query.getAddressValues());
                 addressDoctorScript = scriptMapping.get(detectedScript);
+                addressDoctorScript = addressDoctorScript == null ? scriptMapping.get(null) : addressDoctorScript;
             }
         } else if (query.getPreferredScript() != null) {
             try {

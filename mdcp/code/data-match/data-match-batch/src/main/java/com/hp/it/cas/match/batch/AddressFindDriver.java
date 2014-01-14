49f8d2371a07392a6cf572717cd1cb9813a0b00a
Yu-juan Zhang <yu-juan.zhang@hp.com>
2014-01-14 10:47:23 +0000

    change the file disposition method from move to delete.
    git-svn-id: file:///opt/mount/casfw/ad-git-migration/mdcp-svn/trunk@16936 3ae50711-85f4-4630-9f8a-40507e8d37d8

--- mdcp/code/data-match/data-match-batch/src/main/java/com/hp/it/cas/match/batch/AddressFindDriver.java
+++ mdcp/code/data-match/data-match-batch/src/main/java/com/hp/it/cas/match/batch/AddressFindDriver.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 import com.hp.it.cas.batch.driver.pipe.Configuration;
 import com.hp.it.cas.batch.driver.pipe.DelimitedLineTokenizer;
 import com.hp.it.cas.batch.driver.pipe.FileDisposition;
-import com.hp.it.cas.batch.driver.pipe.FileDispositionMove;
+import com.hp.it.cas.batch.driver.pipe.FileDispositionDelete;
 import com.hp.it.cas.batch.driver.pipe.LineMapper;
 import com.hp.it.cas.batch.driver.pipe.PatternLineMapper;
 import com.hp.it.cas.batch.driver.pipe.TransactionController;
@@ -24,11 +24,9 @@ import com.hp.it.cas.batch.driver.pipe.TransactionFileDriver;
  */
 public class AddressFindDriver extends TransactionFileDriver<AddressFind, Void> {
 	
-	private final Configuration configuration;
 
 	public AddressFindDriver(Configuration configuration) {
 		super(configuration);
-		this.configuration = configuration;
 	}
 
 	@Override
@@ -38,7 +36,7 @@ public class AddressFindDriver extends TransactionFileDriver<AddressFind, Void> 
 
 	@Override
 	protected FileDisposition getFileDisposition() {
-		return new FileDispositionMove(configuration);
+		return new FileDispositionDelete();
 	}
 
 	@Override

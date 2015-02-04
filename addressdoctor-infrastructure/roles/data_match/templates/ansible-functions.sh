6e1662cf1148fb3503cdc252264941afb7cfa0f8
Li-fang.Zhao <li-fang.zhao@hp.com>
2015-02-04 18:30:27 +0800

    change parameter in ansible-function.sh

--- addressdoctor-infrastructure/roles/data_match/templates/ansible-functions.sh
+++ addressdoctor-infrastructure/roles/data_match/templates/ansible-functions.sh
@@ -34,7 +34,7 @@ function finalCleanup {
 }
 
 function restfulEndpointTest {
-    response=$(curl --header "X-HP-Application-Process-UID: w-mdcp:prd-http" -s -i http://{{ inventory_host }}:{{ port }}/match/validatedAddress?country1=US&deliveryAddressLine1=745+Riverhaven+Drive&characterScriptDetectionIndicator=false&postalCode1=30024)
+    response=$(curl --header "X-HP-Application-Process-UID: w-mdcp:prd-http" -s -i http://{{ inventory_hostname }}:{{ port }}/match/validatedAddress?country1=US&deliveryAddressLine1=745+Riverhaven+Drive&characterScriptDetectionIndicator=false&postalCode1=30024)
     if ! echo "${response}" | grep -q "HTTP/1.1 200"
        then
          error "Restful Endpoint test did not return 200 status."

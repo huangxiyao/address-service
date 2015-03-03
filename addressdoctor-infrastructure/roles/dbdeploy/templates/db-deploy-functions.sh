4dbbb7284013a13616fbaa6da5bc429c63100b19
Yu-juan Zhang <yu-juan.zhang@hp.com>
2015-03-03 16:25:44 +0800

    Check disk space and db target folder before copy db files.

--- addressdoctor-infrastructure/roles/dbdeploy/templates/db-deploy-functions.sh
+++ addressdoctor-infrastructure/roles/dbdeploy/templates/db-deploy-functions.sh
@@ -19,6 +19,15 @@ function checkDiskSpace {
     fi
 }
 
+# make sure db target folder exist
+function checkDBTargetFolder {
+    if [[ -d {{ db_target_folder }} ]]; then
+        mv {{ db_target_folder }} {{ db_target_folder }}_"$(date +"%Y-%m-%d-%H:%M:%S")"
+    else
+        mkdir -p {{ db_target_folder }}
+    fi
+}
+
 # Update DB Symlink
 function updateDBSymLink {
     cd {{ casfw_home }}/address-doctor/databases

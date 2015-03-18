2dcc1da31e9c9db671a730c33eff29515faa9435
Li, Hong-Bo <hong-bol@hp.com>
2015-03-18 17:33:50 +0800

    Add acceptance test role.

---
+++ addressdoctor-infrastructure/roles/acceptance-test/templates/acceptance-test-functions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+HOME="/opt/casfw"
+userinput="$1"
+lb_url="$2"
+
+function error {
+    local message=$1
+    echo $1 >&2
+    exit -1
+}
+
+function soapValidationTest {
+    cd /tmp
+
+    checkurl="http://${lb_url}/legacy-match/address/v1?wsdl"
+    response=$(curl --header "Content-Type: text/xml;charset=UTF-8" --header "X-HP-Application-Process-UID: w-mdcp:prd-http" --data @soap_envelope.xml -s -i ${checkurl})
+    if ! echo "${response}" | grep -q "HTTP/1.1 200"
+    then
+      error "Soap Validation test did not return 200 status."
+    fi
+
+    echo "$checkurl - SUCCESS with soap data: "
+    echo "$(cat soap_envelope.xml)"
+}
+
+function finalCleanup {
+    cd /tmp
+    rm -f acceptance-test-functions.sh
+    rm -f soap_envelope.xml
+}
+
+$userinput

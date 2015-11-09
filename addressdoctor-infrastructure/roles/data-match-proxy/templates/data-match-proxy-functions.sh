0668ef4c5b050f14967ae9df1fd1bc4e0f900405
Jaylin <yu-juan.zhang@hpe.com>
2015-11-09 17:38:07 +0800

    Handling apache config for Webhost Apache in data-match-proxy deploy, because the apache instance is /opt/wehbost/WHA-General-Inst on HPE PRO.

--- addressdoctor-infrastructure/roles/data-match-proxy/templates/data-match-proxy-functions.sh
+++ addressdoctor-infrastructure/roles/data-match-proxy/templates/data-match-proxy-functions.sh
@@ -5,21 +5,34 @@ userinput=$1
 datamatchdirname="data-match-{{ data_match_release_version }}"
 datamatchcdifile="data-match-installer-{{ data_match_release_version }}.cdi"
 
-function copyCasfwConfScript {
+function apacheTypeCheck {
     if [ -d /opt/cloudhost ]; then
-		copyScript         
+        echo "cloudhost"
+    elif [ -d /opt/webhost/local ]; then
+        echo "webhost"
     fi
 }
 
-function copyScript {
-	if [ ! -f /etc/httpd/conf.d/casfw.conf ]; then
+function copyCasfwConfToCloudApache {
+    if [ ! -f /etc/httpd/conf.d/casfw.conf ]; then
         /opt/pb/bin/pbrun cp {{ casfw_home }}/casfw.conf /etc/httpd/conf.d/
         /opt/pb/bin/pbrun chmod 644 /etc/httpd/conf.d/casfw.conf
     fi
 }
 
+function copyCasfwConfToWebhostApache {
+    if [ ! -f /opt/webhost/local/WHA-General-Inst/apache/conf.d/casfw.conf ]; then
+        cp {{ casfw_home }}/casfw.conf /opt/webhost/local/WHA-General-Inst/apache/conf.d/
+        chmod 644 /opt/webhost/local/WHA-General-Inst/apache/conf.d/casfw.conf
+    fi
+}
+
 function restartCloudApacheInstance {
 	sudo service httpd restart
+}
+
+function restartWebhostApacheInstance {
+    /opt/webhost/whaeng/bin/restart_apache WHA-General-Inst
 }
 
 function finalCleanup {

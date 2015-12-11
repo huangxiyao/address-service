59d1c26157758a7e65f4a92e65a3ac5a3fb53672
Slawek Zachcial <slawomir.zachcial@hp.com>
2015-12-11 12:24:20 +0100

    Rename data-match script back to tomcat-ad.sh to make it backward compatible.

--- data-match/data-match-installer/src/main/dist/bin/.post-install.sh
+++ data-match/data-match-installer/src/main/dist/bin/.post-install.sh
@@ -15,20 +15,20 @@ ln -sf ${CASFW_HOME} ${CASFW_HOME_CURRENT}/${AD_INSTANCE_NAME}
 # Create init.d folder under ${CASFW_HOME}
 mkdir -p ${CASFW_HOME}/init.d
 # Create the symbol link pointing to the actual shell script
-ln -sf ${CASFW_HOME}/bin/data-match ${CASFW_HOME}/init.d/data-match
+ln -sf ${CASFW_HOME}/bin/tomcat-ad.sh ${CASFW_HOME}/init.d/tomcat-ad.sh
 
 #Must limit jxmremote.* files to read only for the casfw user
 chmod 600 ${CASFW_HOME}/etc/tomcat-ad/conf/jmxremote.password
 chmod 600 ${CASFW_HOME}/etc/tomcat-ad/conf/jmxremote.access
 
 #Because tomcat-ad.sh didn't exist before the installer ran, we must explicitly set his permissions to executable
-chmod ug+x ${CASFW_HOME}/bin/*
+chmod ug+x ${CASFW_HOME}/bin/*.sh
 
 # Print message about URL where tomcat is to run
 tomcat_ad_http_port=$(grep tomcat_ad_connector_http_port ${CASFW_HOME}/etc/casfw.properties | awk -F "=" '{print $2}')
 echo
 echo "Starting Tomcat at http://$(hostname):${tomcat_ad_http_port}"
-${CASFW_HOME}/bin/data-match start &
+${CASFW_HOME}/bin/tomcat-ad.sh start &
 
 echo
 echo "Please check ${CASFW_HOME}/README.txt for details of the components included"

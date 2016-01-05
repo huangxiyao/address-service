145695b37d7fa8e3add7054a180ea9d0587b6a34
Jaylin <yu-juan.zhang@hpe.com>
2016-01-05 13:49:50 +0800

    Add as-monitor-notify.sh script to summary the monitor result and post the failed check urls to Flowdock Inbox.

---
+++ addressdoctor-infrastructure/scripts/as-monitor-notify.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+# ================================================================================
+# Flowdock
+# ================================================================================
+
+ENVIRONMENT="$1"
+FLOW_TOKEN="f15e4c48063baa75b9617d297f60a372" # Jenkins Flowdock Test
+
+RESULTFILE="as-monitor-result.txt";
+FAILEDRECORDS=""
+TABLE=""
+
+if [[ -f $RESULTFILE ]]
+then
+    FAILEDRECORDS=$(cat $RESULTFILE | grep "${JOB_NAME}-${BUILD_NUMBER}")
+fi
+
+function retrieveFailedRecords {
+    TABLE="<table border='1'><tr><th>Failed Url</th><th>Status Code</th></tr>";
+
+while read record
+do
+    url=$(echo ${record} | awk '{print $2}');
+    status=$(echo ${record} | awk '{print $3}');
+    TABLE=${TABLE}"<tr><td><a href="${url}">"${url}"</a></td><td>"${status}"</td></tr>";
+done << EOF
+$FAILEDRECORDS
+EOF
+
+    TABLE=${TABLE}"</table>";
+}
+
+function postFailedRecordsToFlowdockInbox {
+    curl --silent \
+         --header "Accept: application/json" \
+         --form "flow_token=$FLOW_TOKEN" \
+         --form "event=activity" \
+         --form "author[name]=Jenkins" \
+         --form "author[avatar]=http://jenkins-ci.org/sites/default/files/images/headshot.png" \
+         --form "title= <a href=${BUILD_URL}console>${JOB_NAME}-${BUILD_NUMBER} Console Output</a>" \
+         --form "tags=AS-Monitoring" \
+         --form "external_thread_id=extract:as:monitor:$ENVIRONMENT" \
+         --form "thread[title]=$JOB_NAME" \
+         --form "thread[status][color]=orange" \
+         --form "thread[status][value]=failure" \
+         --form "body= ${TABLE}" \
+         "https://api.flowdock.com/messages" > /dev/null
+}
+
+if [[ $FAILEDRECORDS != "" ]]
+then
+    echo "Sending out the FAILED Monitor notification..."
+    retrieveFailedRecords
+    postFailedRecordsToFlowdockInbox
+    exit 1
+else
+    echo "Monitors are SUCCESS!"
+    exit 0
+fi

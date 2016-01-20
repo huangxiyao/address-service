2b1850f67bef4b28d66b515a9976bd644812dd95
Jaylin <yu-juan.zhang@hpe.com>
2016-01-20 14:57:34 +0800

    Add SSL Check with jrunscript and curl command.

--- addressdoctor-infrastructure/roles/acceptance-test/files/acceptance-test-functions.sh
+++ addressdoctor-infrastructure/roles/acceptance-test/files/acceptance-test-functions.sh
@@ -3,6 +3,7 @@
 HOME="/opt/casfw"
 userinput="$1"
 lb_url="$2"
+health_check_url="$3"
 
 function error {
     local message=$1
@@ -43,6 +44,29 @@ EOF
     fi
 
     echo "$checkurl - SUCCESS with soap data: "
+}
+
+function jrunscriptTest {
+    checkurl="https://${lb_url}/${health_check_url}"
+    status=$(jrunscript -e "print(new java.net.URL(\"${checkurl}\").openConnection().getResponseCode())")
+
+    if [[ ${status} -ne 200 ]]
+    then
+      error "$checkurl - jrunscript command test did not return 200 status."
+    fi
+
+    echo "$checkurl - SUCCESS with jrunscript command test"
+}
+
+function curlTest {
+    checkurl="https://${lb_url}/match/validatedAddress?country1=US&deliveryAddressLine1=\"11925 Jones RD\"&locality1=Houston&postalCode1=77070&province1=TX"
+    status=$(curl -o "/dev/null" -s -w "%{http_code}" -H "X-HP-Application-Process-UID:hpit:CD-TEST" "${checkurl}")
+    if [[ ${status} -ne 200 ]]
+    then
+      error "$checkurl - curl test did not return 200 status."
+    fi
+
+    echo "$checkurl - SUCCESS with curl command test."
 }
 
 $userinput
